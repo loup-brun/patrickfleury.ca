ea062d152cf7dc854957d436cc33d2b15afe67dd
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-12-23 23:55:54 -0500

    Corriger carousel sur about, ajouter ::selection style

--- web/app/themes/pfleury-wordpress/about.php
+++ web/app/themes/pfleury-wordpress/about.php
@@ -36,10 +36,7 @@ get_header();
       <?php the_content(); ?>
     </section>
 
-    <?php if (get_field('show_testimonials')): ?>
-    <section class="py-5" id="testimonials">
-      <h3 class="text-center"><?php _e('What clients are saying', 'pfleury-wordpress'); ?></h3>
-      <?php
+    <?php if (get_field('show_testimonials')) {
       // Query testimonials
       $testimonialsQuery = array();
 
@@ -50,45 +47,53 @@ get_header();
       }
 
       if ($testimonialsQuery->have_posts()) { ?>
-      <div class="swipe-js">
-        <div class="swipe-js-wrap d-flex flex-row">
-          <?php while ($testimonialsQuery->have_posts()) : $testimonialsQuery->the_post(); ?>
+      <section class="py-5" id="testimonials">
+        <h3 class="text-center"><?php _e('What clients are saying', 'pfleury-wordpress'); ?></h3>
+        <div class="swipe-js">
+          <div class="swipe-js-wrap d-flex flex-row">
+            <?php while ($testimonialsQuery->have_posts()) : $testimonialsQuery->the_post(); ?>
 
-          <section class="swipe-js-slide d-flex flex-column justify-content-center">
-            <div class="container d-flex justify-content-center">
-              <blockquote class="big-quote">
-                <?php the_content(); ?>
-                <?php if (get_field('testimony_author')): ?>
-                <cite><?php echo get_field('testimony_author'); ?>
-                  <?php if (get_field('testimony_company')): ?>
-                    <br><span class="font-weight-normal"><?php echo get_field('testimony_company'); ?></span>
+            <section class="swipe-js-slide d-flex flex-column justify-content-center">
+              <div class="container d-flex justify-content-center">
+                <blockquote class="big-quote">
+                  <?php the_content(); ?>
+                  <?php if (get_field('testimony_author')): ?>
+                  <cite><?php echo get_field('testimony_author'); ?>
+                    <?php if (get_field('testimony_company')): ?>
+                      <br><span class="font-weight-normal"><?php echo get_field('testimony_company'); ?></span>
+                    <?php endif; ?>
+                  </cite>
                   <?php endif; ?>
-                </cite>
-                <?php endif; ?>
-              </blockquote>
-            </div>
-          </section>
+                </blockquote>
+              </div>
+            </section>
 
-          <?php endwhile; ?>
+            <?php endwhile; ?>
+          </div>
+          <?php if (sizeof($testimonialsQuery->posts) > 1): ?>
+          <button class="swipe-js-btn -prev">
+            <div class="icon icon-arrow-left"></div>
+          </button>
+          <button class="swipe-js-btn -next">
+            <div class="icon icon-arrow-right"></div>
+          </button>
+          <nav class="swipe-js-nav text-center">
+          </nav>
+          <?php endif; ?>
         </div>
-        <?php if (sizeof($testimonialsQuery->posts) > 1): ?>
-        <button class="swipe-js-btn -prev">
-          <div class="icon icon-arrow-left"></div>
-        </button>
-        <button class="swipe-js-btn -next">
-          <div class="icon icon-arrow-right"></div>
-        </button>
-        <nav class="swipe-js-nav text-center">
-        </nav>
-        <?php endif; ?>
-      </div>
-      <?php } else  { ?>
-      <div class="text-center">
-        No slides!
-      </div>
-      <?php } ?>
-    </section>
-    <?php endif; // end testimonials ?>
+      </section>
+    <?php
+      } // end if query
+    } // end testimonials
+    ?>
+
+  <?php if (get_field('about_outro')): ?>
+  <section class="content">
+    <?php
+    the_field('about_outro');
+    ?>
+  </section>
+  <?php endif; ?>
   </article>
 
   <?php endwhile // end of the loop. ?>
